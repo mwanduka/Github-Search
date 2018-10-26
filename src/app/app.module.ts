b43e0c46808ca70681988d21c3febff9c60f30ca
mwanduka <mwandukastephen20@gmail.com>
2018-10-26 14:13:19 +0300

    add new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubFormComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/quote-http/quote-request.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/core';
+import {Environment} from '../../environment/environment'
+import {User} from '../user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteRequestService {
+
+  constructor() { }
+}
